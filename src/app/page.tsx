da0e00d92bc7c1effca5bab5b5005e28c6fd9226
Ankit singh <ankitsinghmyself@gmail.com>
2024-02-02 11:37:49 +0530

    new update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,23 +40,15 @@ export default function Home() {
   return (
     <>
       <PageLayout title="Dashboard">
-        <div className="p-5 md:p-10 grid grid-cols-1 md:grid-cols-3 gap-4 bg-gradient-to-r from-gray-500 to-red-500 rounded-md">
+        <div className="p-5 md:px-10 grid grid-cols-1 md:grid-cols-3 gap-4 bg-gradient-to-r from-gray-500 to-red-500 rounded-md">
           <div className="col-span-2  flex items-center ">
-            <motion.div
-              initial={{ opacity: 0, scale: 0.5 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{ duration: 2 }}
-            >
-              <Image
-                src="/ankit.jpg"
+              <img
+                src="/ankit.png"
                 alt="Profile Pic"
-                width={100}
-                height={50}
-                className=" rounded-full border-white border-4 border-solid"
+                className=" w-1/5 h-auto rounded-full border-white border-4 border-solid"
               />
-            </motion.div>
             <div className="ml-5 flex flex-col ">
-              <h3 className="text-2xl font-bold text-gray-300">Ankit Singh</h3>
+              <h3 className="text-3xl font-bold text-white">Ankit Singh</h3>
               <p className="text-sm">Software Engineer</p>
             </div>
           </div>
@@ -88,34 +80,36 @@ export default function Home() {
 
         <div className="p-5  md:p-10 grid grid-cols-1 md:grid-cols-4 gap-4  ">
           {project.map((item, index) => (
-            <div
-              key={index}
-              className="max-w-sm bg-gray-800 border border-gray-200 rounded-lg shadow  dark:border-gray-700"
-            >
-              <a href={item.url} target="_blank">
-                <img
-                  className="rounded-t-lg"
-                  src={item.image}
-                  alt={item.title}
-                  height={200}
-                  width={500}
-                  style={{
-                    width: "100%",
-                    aspectRatio: 1 / 1,
-                    height: "170px",
-                    objectFit: "cover",
-                  }}
-                />
-              </a>
-              <div className="p-5">
-                <a href={item.url}>
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
-                    {item.title}
-                  </h5>
+            <div key={index} className="">
+              <div className="rounded-t-xl overflow-hidden relative">
+                <a href={item.url} target="_blank">
+                  <img
+                    className="opacity-80 w-full h-full object-cover"
+                    src={item.image}
+                    alt={item.title}
+                    height={200}
+                    width={500}
+                    style={{
+                      width: "100%",
+                      aspectRatio: 1 / 1,
+                      height: "170px",
+                      objectFit: "cover",
+                    }}
+                  />
+                   {/* <div className="overlay-text absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+                      <h5 className="mb-2 text-md font-bold text-white">{item.title}</h5>
+                    </div> */}
                 </a>
-                <p className="mb-3 font-normal text-gray-400 ">
-                  {item.description}
-                </p>
+                <div className="p-5 bg-white  rounded-b-xl overflow-hidden">
+                  <a href={item.url}>
+                    <h5 className="mb-2 text-md font-bold text-black">
+                      {item.title}
+                    </h5>
+                  </a>
+                  <p className="mb-3 text-xs font-normal text-black ">
+                    {item.description}
+                  </p>
+                </div>
               </div>
             </div>
           ))}
@@ -126,7 +120,7 @@ export default function Home() {
           <p className="text-white text-2xl font-bold">What I know</p>
         </div>
         <div className="p-5  md:p-10 grid grid-cols-1 md:grid-cols-5 gap-4  ">
-          <div className="max-w-sm bg-gray-800 border border-gray-200 rounded-lg shadow  dark:border-gray-700">
+          <div className="max-w-sm bg-gray-800   rounded-lg shadow  dark:border-gray-700">
             <div className="p-5">
               <h5 className="mb-2 text-[1.125rem] font-medium tracking-tight text-white">
                 Frontend
@@ -180,10 +174,18 @@ export default function Home() {
                 >
                   React Js
                 </div>
+                <div
+                  data-te-chip-init
+                  data-te-ripple-init
+                  className="[word-wrap: break-word] my-[5px] mr-4 flex h-[32px] border-red-300 border-[1px]  items-center justify-between rounded-md bg-dark px-[8px] py-0 text-[.75rem] font-normal normal-case leading-loose text-gray-400 shadow-none transition-[opacity] duration-300 ease-linear hover:!shadow-none "
+                  data-te-close="true"
+                >
+                  NextJS 13
+                </div>
               </div>
             </div>
           </div>
-          <div className="max-w-sm bg-gray-800 border border-gray-200 rounded-lg shadow  dark:border-gray-700">
+          <div className="max-w-sm bg-gray-800  rounded-lg shadow  dark:border-gray-700">
             <div className="p-5">
               <h5 className="mb-2 text-[1.125rem] font-medium tracking-tight text-white">
                 Backend
@@ -208,7 +210,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="max-w-sm bg-gray-800 border border-gray-200 rounded-lg shadow  dark:border-gray-700">
+          <div className="max-w-sm bg-gray-800  rounded-lg shadow  dark:border-gray-700">
             <div className="p-5">
               <h5 className="mb-2 text-[1.125rem] font-medium tracking-tight text-white">
                 Database
@@ -233,7 +235,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="max-w-sm bg-gray-800 border border-gray-200 rounded-lg shadow  dark:border-gray-700">
+          <div className="max-w-sm bg-gray-800  rounded-lg shadow  dark:border-gray-700">
             <div className="p-5">
               <h5 className="mb-2 text-[1.125rem] font-medium tracking-tight text-white">
                 Devops
