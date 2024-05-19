838f451b6d1574298a513f1c4e363c5c746548cd
Ankit singh <ankitsinghmyself@gmail.com>
2024-05-19 21:59:07 +0530

    Added PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,52 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const APP_NAME = "Ankit Singh";
+const APP_DEFAULT_TITLE = "My Awesome PWA Web App";
+const APP_TITLE_TEMPLATE = "%s - Ankit SIngh";
+const APP_DESCRIPTION = "Best PWA app in the world!";
+
 export const metadata: Metadata = {
-  title: "Ankit Singh",
-  description: "Full Stack Developer",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,17 @@ import PageLayout from "@/components/PageLayout";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import { color, motion, useAnimation } from "framer-motion";
-import Image from "next/image";
-import Link from "next/link";
 import { useEffect } from "react";
 import { SiLinkedin } from "react-icons/si";
+
 export default function Home() {
   return (
     <>
       <PageLayout title="Dashboard">
         <Intro />
         <Skills />
-
         <Projects />
+
       </PageLayout>
     </>
   );
