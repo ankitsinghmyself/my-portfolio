c93cbb13bee6713b9d1d203e15e63630d0c82115
Ankit Singh <ankitsinghmyself@gmail.com>
2024-10-09 22:53:35 +0530

    fix

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -7,28 +7,8 @@ import ThreeDImage from "./common/ThreeDImage";
 const Intro: React.FC = () => {
   return (
     <div className="p-5 md:px-10 flex flex-col md:flex-row items-center justify-between bg-gradient-to-r from-gray-500 to-red-500 rounded-md">
-      <div className="flex flex-col md:flex-row items-center justify-center gap-4 mb-5 md:mb-0">
-        {/* <img
-          src="/ankit.png"
-          alt="Profile Pic"
-          className="w-24 h-24 md:w-32 md:h-32 rounded-full border-white border-4 border-solid"
-        /> */}
-        <ThreeDImage
-          src="/ankit.png"
-          alt="Ankit singh"
-          width={200}
-          height={180}
-        />
-
-        <p className="text-sm text-white text-center md:text-left">
-          <span className="text-2xl md:text-3xl font-bold text-white mb-2 block">
-            Ankit Singh
-          </span>
-          Software Engineer
-        </p>
-      </div>
       <div className="flex flex-col items-center md:items-start">
-        <CustomButton
+        {/* <CustomButton
           label="Follow me on LinkedIn"
           icon={SiLinkedin}
           onClick={() =>
@@ -37,7 +17,23 @@ const Intro: React.FC = () => {
               "_blank"
             )
           }
-        />
+        /> */}
+      </div>
+      <div className="flex flex-col items-center justify-center gap-4 mb-5 md:mb-0 md:flex-row">
+        <p className="text-sm text-white text-center md:text-left order-2 md:order-1">
+          <span className="text-2xl md:text-3xl font-bold text-white mb-2 block">
+            Ankit Singh
+          </span>
+          Software Engineer
+        </p>
+        <div className="order-1 md:order-1">
+          <ThreeDImage
+            src="/ankit.png"
+            alt="Ankit Singh"
+            width={200}
+            height={180}
+          />
+        </div>
       </div>
     </div>
   );
